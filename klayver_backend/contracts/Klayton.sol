b48cdde75127b92d41c9e3b685a18ebdda2619fa
ebbieaden <ebbieaden@gmail.com>
2023-10-25 01:40:07 +0100

    changes made to klayton

--- klayver_backend/contracts/Klayton.sol
+++ klayver_backend/contracts/Klayton.sol
@@ -8,6 +8,9 @@ contract Klayton is ERC20, Ownable {
     mapping(address => uint256) public disputedFunds;
     address public admin;
 
+    event FundsWithheld(address account, uint256 amount);
+    event FundsReleased(address account, uint256 amount);
+
     constructor() ERC20("Klayton", "KLY") {
         _mint(msg.sender, 200_000_000 * 10 ** uint256(decimals()));
         admin = msg.sender;      
@@ -22,16 +25,19 @@ contract Klayton is ERC20, Ownable {
     function withHoldFunds(address account, uint256 amount)  public onlyAdmin {
         require (balanceOf(account) >= amount, "Insufficient balance to withhold");
         disputedFunds[account] += amount;
+        emit FundsWithheld(account, amount);
     }
 
     function releaseFunds(address account, uint256 amount) public onlyAdmin{
         require(disputedFunds[account] >= amount, "Not enough disputed funds");
         disputedFunds[account] -= amount;
         _transfer(address(this), account, amount);
+        emit FundsReleased(account, amount);
     }
 
     // function to change admin's address
     function changeAdmin(address newAdmin) public onlyOwner {
+        require(newAdmin != address(0), "The new admin address cannot be zero");
         admin = newAdmin;
     }
 
@@ -39,7 +45,5 @@ contract Klayton is ERC20, Ownable {
        _approve(msg.sender, spender, value);
        return true;
     }
-    // function allowance(address owner, address spender) public override view returns (uint256 remaining) {
-    //     return allowed[owner][spender];
     
 }
