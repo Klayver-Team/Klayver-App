0291f2f757e5129edacefd5257880af65db2b18b
ebbieaden <ebbieaden@gmail.com>
2023-11-24 00:29:35 +0100

    minor changes made

--- klayver_backend/scripts/deploy.js
+++ klayver_backend/scripts/deploy.js
@@ -3,25 +3,25 @@ const { ethers } = require("hardhat");
 
 async function main() {
 
-  const deployerAddr = "";
+  const deployerAddr = "0xa44C7014aE6A4f065086dbD5417272DBA1f489f0";
   const deployer = await ethers.getSigner(deployerAddr);
 
   console.log(`Deploying Klayver contract with the account ${deployer.address}`);
   console.log(`Account balance: ${(await deployer.provider.getBalance(deployerAddr)).toString()}`)
 
   const klayver = await ethers.deployContract("Klayver");
-  const klayverProfile = await ethers.deployContract("KlayProfile");
+  const klayProfile = await ethers.deployContract("KlayverProfile");
   const klayverToken = await ethers.deployContract("KlayverToken");
 
   console.log("Deploying Klayver");
   await klayver.waitForDeployment();
   console.log("Deploying KlayProfile");
-  await klayverProfile.waitForDeployment()
+  await klayProfile.waitForDeployment()
   console.log("Deploying KlayverToken");
   await klayverToken.waitForDeployment();
 
   console.log(`Klayver successfully deployed ${klayver.address}`);
-  console.log(`KlayProfile successfully deployed ${klayverProfile.address}`);
+  console.log(`KlayProfile successfully deployed ${klayProfile.address}`);
   console.log(`KlayverToken successfully deployed ${klayverToken.address}`);
   console.log("Deployment completed!")
 }
